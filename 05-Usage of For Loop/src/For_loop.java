3daf54e606fdf3b09bbe56d7486bb24f290937c0
Burakegekocabay <burakegekocabay@outlook.com>
2024-03-17 18:28:19 +0300

    For loop solution

---
+++ 05-Usage of For Loop/src/For_loop.java
@@ -0,0 +1,10 @@
+public class For_loop
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 100; i++) 
+        {
+            System.out.println("I'm learning java");    
+        }
+    } 
+}
